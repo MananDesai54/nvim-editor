eed34a4951647dfa1e20db75b21a116453e3b770
manandesai54 <manan5401desai@gmail.com>
2024-05-02 09:38:58 +0530

    Neo vim starter

---
+++ init.lua
@@ -0,0 +1,38 @@
+vim.cmd("set number")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle left<CR>')
+
+local tree_sitter_configs = require("nvim-treesitter.configs")
+tree_sitter_configs.setup({
+  ensure_installed = { "lua", "javascript", "python" },
+  sync_install = true,
+  highlight = { enable = true },
+  indent = { enable = true }
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
