d17ad0cc1de701087db740a852b793e6e963db11
mdesai-ontic <mdesai@ontic.co>
2024-05-03 20:51:00 +0530

    adds linter and formatter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,9 @@
 return {
-  "catppuccin/nvim", 
-  name = "catppuccin", 
-  priority = 1000, 
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
   lazy = false,
-  config = function ()
-    vim.cmd.colorscheme "catppuccin"
-  end
+  config = function()
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
   config = function()
     local tree_sitter_configs = require("nvim-treesitter.configs")
     tree_sitter_configs.setup({
-      ensure_installed = { "lua", "javascript", "python" },
+      -- ensure_installed = { "lua", "javascript", "python" },
+      auto_install = true,
       sync_install = true,
       highlight = { enable = true },
       indent = { enable = true }
